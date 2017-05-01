f228d3c4bef8b9f723dde0cc8d1bdf5527a53a95
stebogit <stefano.borghi@gmail.com>
2017-04-30 23:07:21 -0700

    removed test

--- test/types.ts
+++
@@ -1,7 +0,0 @@
-import {BBox} from '@turf/helpers'
-import * as pointGrid from '@turf/point-grid'
-import * as matrixToGrid from '../'
-
-const bbox: BBox = [-95, 30, -85, 40]
-const points = pointGrid(bbox, 50, 'miles')
-const array = matrixToGrid(points)
